e54f296742a85fee0a4d5a76688af75dfadb72ba
Chef Brams <oldcitycatering@gmail.com>
2020-10-11 18:01:12 -0400

    updated js

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,40 @@
 // array of questions for user
-const questions = [
+// const questions = [
 
-];
+// ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+// }
 
 // function call to initialize program
-init();
+// init();
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const writeFile = util.promisify(fs.writeFile);
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the project title?",
+            name: "title",
+        }
+
+    ])
+    .then(function (response) {
+        const mdFile = `# ${response.title}
+        `;
+        writeFile("README.md", mdFile)
+            .then(() => console.log("sucess!"))
+            .catch((err) => console.log(err));
+
+
+    });
