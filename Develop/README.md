4a56524ffff4fe6fc87a769ac78a38751b9e0851
Chef Brams <oldcitycatering@gmail.com>
2020-10-12 14:04:55 -0400

    prompts

---
+++ Develop/README.md
@@ -0,0 +1,27 @@
+# readme
+        
+## Discription 
+reame
+        
+## Table of Contents 
+* readme
+        
+## Installation 
+readme
+        
+## Usage 
+readme
+        
+## License 
+readme
+        
+## Contributing 
+readme
+        
+## Tests 
+readme
+        
+## Questions 
+readme
+
+        
--- Develop/index.js
+++ Develop/index.js
@@ -26,14 +26,71 @@ inquirer
             type: "input",
             message: "What is the project title?",
             name: "title",
+        },
+        {
+            type: "input",
+            message: "What is the description of your project?",
+            name: "description",
+
+        },
+        {
+            type: "input",
+            message: "List the table of contents?",
+            name: "contents",
+
+        },
+        {
+            type: "input",
+            message: "What is the installation process?",
+            name: "installation",
+
+        },
+        {
+            type: "input",
+            message: "Describe the usage of the application?",
+            name: "usage",
+
+        },
+        {
+            type: "input",
+            message: "What license are you using?",
+            name: "license",
+
+        },
+        {
+            type: "input",
+            message: "List the contributors?",
+            name: "contribute",
+
+        },
+        {
+            type: "input",
+            message: "What Tests did you run on the product?",
+            name: "tests",
+
+        },
+        {
+            type: "input",
+            message: "Any questions for the user?",
+            name: "questions",
+
         }
 
     ])
     .then(function (response) {
         const mdFile = `# ${response.title}
+        \n## Discription \n${response.description}
+        \n## Table of Contents \n* ${response.contents}
+        \n## Installation \n${response.installation}
+        \n## Usage \n${response.usage}
+        \n## License \n${response.license}
+        \n## Contributing \n${response.contribute}
+        \n## Tests \n${response.tests}
+        \n## Questions \n${response.questions}
+
         `;
         writeFile("README.md", mdFile)
-            .then(() => console.log("sucess!"))
+            .then(() => console.log("Sucess!"))
             .catch((err) => console.log(err));
 
 
